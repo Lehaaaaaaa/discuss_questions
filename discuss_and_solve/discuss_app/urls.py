99e9fab42e2836459501e7e572245431a30a956d
Lehaaaaaaaa <kirsanovlesha832@gmail.com>
2026-06-06 21:24:08 +0300

    система лайков, избранных, возможность удалить пост и поменять состояние вопроса

--- discuss_and_solve/discuss_app/urls.py
+++ discuss_and_solve/discuss_app/urls.py
@@ -6,6 +6,10 @@ urlpatterns= [
     path('', views.index, name = 'index_page'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('ask/', views.create_question, name = 'create_question')
-
+    path('ask/', views.create_question, name = 'create_question'),
+    path('question/<int:question_id>/delete/', views.delete_question, name = 'delete_question'),
+    path('question/<int:question_id>/toggle/', views.toggle_resolve, name='toggle_resolve'),
+    path('question/<int:question_id>/like/', views.toggle_like, name='toggle_like'),
+    path('question/<int:question_id>/favorite/', views.toggle_favorite, name='toggle_favorite'),
+    path('favorites/', views.favorite_questions, name='favorite_questions')
 ]
--- discuss_and_solve/discuss_app/views.py
+++ discuss_and_solve/discuss_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Question
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Question, Favorite
 from django.contrib.auth.decorators import login_required
 from django import forms
 from django.utils.text import slugify
@@ -12,7 +12,14 @@ class QuestionForm(forms.ModelForm):
 
 def index(request):
     all_questions = Question.objects.all().order_by('-created_at')
-    return render(request, 'index.html', {'questions':all_questions})
+    user_favorites_ids = []
+    if request.user.is_authenticated:
+        user_favorites_ids = Favorite.objects.filter(user=request.user).values_list('question_id', flat=True)
+        
+    return render(request, 'index.html', {
+        'questions': all_questions, 
+        'user_favorites_ids': user_favorites_ids
+    })
 
 @login_required(login_url = 'login')
 def create_question(request):
@@ -28,4 +35,51 @@ def create_question(request):
     
     return render(request, 'create_question.html', {'form':form})
 
+@login_required
+def delete_question(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    if question.author == request.user:
+        question.delete()
+    return redirect('index_page')
 
+@login_required
+def toggle_resolve(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    if question.author == request.user:
+        question.is_solved = not question.is_solved
+        question.save()
+    return redirect(request.META.get('HTTP_REFERER', 'index_page'))
+
+@login_required
+def toggle_like(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+
+    if request.user in question.likes.all():
+        question.likes.remove(request.user)
+    else:
+        question.likes.add(request.user)
+    
+    return redirect(request.META.get('HTTP_REFERER', 'index_page'))
+
+@login_required
+def toggle_favorite(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    favorite_record = Favorite.objects.filter(user = request.user, question = question)
+
+    if favorite_record.exists():
+        favorite_record.delete()
+    else:
+        Favorite.objects.create(user=request.user, question = question)
+    return redirect(request.META.get('HTTP_REFERER', 'index_page'))
+
+
+@login_required
+def favorite_questions(request):
+    favorites = Favorite.objects.filter(user=request.user).select_related('question')
+    questions = [fav.question for fav in favorites]
+    user_favorites_ids = [q.id for q in questions]
+    
+    return render(request, 'favorites.html', {
+        'questions': questions,
+        'user_favorites_ids': user_favorites_ids
+    })
